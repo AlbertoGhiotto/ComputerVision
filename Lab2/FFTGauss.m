69e85f3cb3633b0d38d1a451ec7a148dde859290
Alberto <alberto.ghiotto@hotmail.it>
2019-03-26 11:36:44 +0100

    Corrected last figure caption and put size as second meshgrid parameter in FFTGauss function

--- Lab2/FFTGauss.m
+++ Lab2/FFTGauss.m
@@ -1,7 +1,7 @@
 function Fimg = FFTGauss(sigma, size, imgShow)
 
 %Define a grid
-[X,Y] = meshgrid(1:101);
+[X,Y] = meshgrid(1:size);
 
 %Gaussian
 Z = exp((-X.^2 -Y.^2) / (2 * (sigma ^ 2)));
@@ -10,5 +10,5 @@ Z = exp((-X.^2 -Y.^2) / (2 * (sigma ^ 2)));
 Fimg = fftshift( fft2( Z));
 
 if(imgShow)
-   figure, imagesc(log(abs(Fimg))), colormap gray, xlabel('wx'), ylabel('wy'), title('log(abs(FFTtransform)) of 2D-Gaussian') 
+   figure, imagesc(log(abs(Fimg))), colormap gray, xlabel('wx'), ylabel('wy'), title('log(abs(FFTtransform)) of a 2D-Gaussian filter') 
 end

c15fe92262033bc3e6b19fa03aa4917fe06038cf
Unknown <nico.decarli@live.it>
2019-03-26 11:22:58 +0100

    Correction in  the median filter

--- Lab2/MedianFilter.m
+++ Lab2/MedianFilter.m
@@ -6,7 +6,7 @@ imgFiltered = medfilt2(img, [support, support]);
 if(imgShow)
     figure
     subplot(2,2,1), imagesc(img), colormap gray, title([imgType, ' noisy image'])
-    subplot(2,2,2),imagesc(imgFiltered),colormap gray,title({'smoothing by median filter'; ['of the ', imgType, ' noisy image']})
+    subplot(2,2,2),imagesc(imgFiltered),colormap gray,title({['smoothing by median filter with support: ', num2str(support)]; ['of the ', imgType, ' noisy image']})
     subplot(2,2,3), imhist(uint8(img),256), title([imgType, ' noisy image histogram'])
     subplot(2,2,4), imhist(uint8(imgFiltered),256), title({['filtered ', imgType]; ' noisy image histogram'})
 end

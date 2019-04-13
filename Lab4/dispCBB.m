c993a5186fc499d70a99a12fb231f9b0140f4929
CurtiClaudio <curti.claudio96@gmail.com>
2019-04-13 16:33:48 +0200

    fixed centroid

--- Lab4/dispCBB.m
+++ Lab4/dispCBB.m
@@ -18,7 +18,7 @@ for i=1:numLab
     end
 end
 
-%To display the results
+%Display the results
 if(doPlot)
     %Find vertex for rectangle 1
     [r1, c1] = find(lab == car1);
@@ -26,9 +26,22 @@ if(doPlot)
     max_r1 = max(r1); 
     min_c1 = min(c1);
     max_c1 = max(c1);
-    %Find coordinates of the first centrode
-    xc1 = floor((min_c1+max_c1)/2);
-    yc1 = floor((min_r1+max_r1)/2);
+    
+    %Find the centroid 1
+    pix = 0; %Number of pixel to 1
+    xc1 = 0; %X coordinate of the centroid 1
+    yc1 = 0; %Y coordinate of the centroid 1
+    for i=min_r1:max_r1
+        for k=min_c1:max_c1
+            if(seg(i, k) == 1)
+                xc1 = xc1 + k;
+                yc1 = yc1 + i;
+                pix = pix + 1;
+            end
+        end
+    end
+    xc1 = floor(xc1/pix);
+    yc1 = floor(yc1/pix);
     
     %Find vertex for rectangle 2
     [r2, c2] = find(lab == car2);
@@ -36,29 +49,43 @@ if(doPlot)
     max_r2 = max(r2); 
     min_c2 = min(c2);
     max_c2 = max(c2);
-    %Find coordinates of the second centrode
-    xc2 = floor((min_c2+max_c2)/2);
-    yc2 = floor((min_r2+max_r2)/2);
+    
+    %Find the centroid 2
+    pix = 0; %Number of pixel to 1
+    xc2 = 0; %X coordinate of the centrode 2
+    yc2 = 0; %Y coordinate of the centrode 2
+    for i=min_r2:max_r2
+        for k=min_c2:max_c2
+            if(seg(i, k) == 1)
+                xc2 = xc2 + k;
+                yc2 = yc2 + i;
+                pix = pix + 1;
+            end
+        end
+    end
+    xc2 = floor(xc2/pix);
+    yc2 = floor(yc2/pix);
     
     %Display all
-    figure, sgtitle('Red cars detected in...')
-    
-    subplot(1, 2, 1), imagesc(imgOrig), title('...original image')
+    figure, sgtitle('Red cars detected:')
+    subplot(1, 2, 1), imagesc(imgOrig), title('Original image')
     hold on;
     %Display the first box
-    plot(xc1,yc1,'*r')
     rectangle('Position', [min_c1, min_r1, max_c1-min_c1, max_r1-min_r1], 'EdgeColor', [1, 0, 0])
     %Display the second box
-    plot(xc2,yc2,'*r')
     rectangle('Position', [min_c2, min_r2, max_c2-min_c2, max_r2-min_r2], 'EdgeColor', [1, 0, 0])
+    %Display the centroids
+    plot(xc1,yc1,'*r'), plot(xc2,yc2,'*r')
     hold off;
-    
-    subplot(1, 2, 2), imagesc(seg), colormap gray, title('...segmented image')
+ 
+    subplot(1, 2, 2), imagesc(seg), colormap gray, title('Segmented image')
     hold on;
-    plot(xc1,yc1,'*r')
+    %Display the first box
     rectangle('Position', [min_c1, min_r1, max_c1-min_c1, max_r1-min_r1], 'EdgeColor', [1, 0, 0])
-    plot(xc2,yc2,'*r')
+    %Display the second box
     rectangle('Position', [min_c2, min_r2, max_c2-min_c2, max_r2-min_r2], 'EdgeColor', [1, 0, 0])
+    %Display the centroids
+    plot(xc1,yc1,'*r'), plot(xc2,yc2,'*r')
     hold off;
 end
 end

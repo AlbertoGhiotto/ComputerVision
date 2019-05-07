8453ca1d6f58b79f77e938ea89a8f66e95b8c013
Alberto <alberto.ghiotto@hotmail.it>
2019-05-07 11:09:15 +0200

    Moved computational analysis in a dedicated function and modified Script accordingly

--- Lab5/Script.m
+++ Lab5/Script.m
@@ -11,10 +11,11 @@ img4 = imread('ur_c_s_03a_01_L_0379.png', 'png');
 img5 = imread('ur_c_s_03a_01_L_0380.png', 'png');
 img6 = imread('ur_c_s_03a_01_L_0381.png', 'png');
 
+
 %% Part 1 (Template matching)
 
-%Template selection
-%Upper-left vertex
+%First template selection
+%Top-left vertex
 xvl = 685; 
 yvl = 352;
 %Bottom-right vertex
@@ -25,61 +26,45 @@ yvu = 430;
 img1gray = rgb2gray(img1);
 %Take the part of the image of interest
 template = img1gray(yvl:yvu, xvl:xvu, :);
+figure, imagesc(template), title('Template [89x78]'), colormap gray
 
-figure, imagesc(template), title('Template')
-
-%Start a timer
-t = cputime;
 corrDetection(img1, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 89x78 is %d', e);
-disp(X)
 corrDetection(img2, template, imgShow);
 corrDetection(img3, template, imgShow);
 corrDetection(img4, template, imgShow);
 corrDetection(img5, template, imgShow);
 corrDetection(img6, template, imgShow);
 
+
 %% Part 2 (Computational comparisons)
 
-%Start a timer
-t = cputime;
-corrDetection(img1, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 89x78 is %d', e);
-disp(X)
+%First template evaluation
+%Top-left vertex
+xvl = 685; 
+yvl = 352;
+%Bottom-right vertex
+xvu = 774;
+yvu = 430;
 
+compTime(yvl, yvu, xvl, xvu, img1 );
 
-%Upper-left vertex
+%Second template evaluation
+%Top-left vertex
 xvl = 675; 
 yvl = 342;
 %Bottom-right vertex
 xvu = 784;
 yvu = 440;
-template = img1gray(yvl:yvu, xvl:xvu, :);
 
-%Start a timer
-t = cputime;
-corrDetection(img6, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 89x78 is %d', e);
-disp(X)
+compTime(yvl, yvu, xvl, xvu, img1 );
 
-%Upper-left vertex
+%Third template evaluation
+%Top-left vertex
 xvl = 665; 
 yvl = 332;
 %Bottom-right vertex
 xvu = 794;
 yvu = 450;
-template = img1gray(yvl:yvu, xvl:xvu, :);
 
-%Start a timer
-t = cputime;
-corrDetection(img6, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 89x78 is %d', e);
-disp(X)
+compTime(yvl, yvu, xvl, xvu, img1 );
+
---
+++ Lab5/compTime.m
@@ -0,0 +1,27 @@
+function compTime(yvl, yvu, xvl, xvu, img )
+
+imgShow = false;
+imgGray = rgb2gray(img);
+
+template = imgGray(yvl:yvu, xvl:xvu, :);
+x = abs(xvl-xvu);
+y = abs(yvl-yvu);
+figure, imagesc(template), title(['Template [', num2str(x) , 'x' , num2str(y) , ']']), colormap gray
+
+%Start a timer
+N = 10;
+
+t = cputime;
+
+ for i=1:N  % Make N cycle and average the result in order to have more precision
+     corrDetection(img, template, imgShow);
+ end
+     
+%Time elapsed
+e = (cputime - t)/N;
+X = sprintf(['The computational time for a template ', num2str(x) , 'x' , num2str(y) , ' is %d'], e);
+disp(X);
+
+
+end
+

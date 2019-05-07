4f8488e70235eeaa27e49cf896abc27598dd0353
Alberto <alberto.ghiotto@hotmail.it>
2019-05-07 11:05:35 +0200

    Modified script

--- Lab5/Script.m
+++ Lab5/Script.m
@@ -38,58 +38,33 @@ corrDetection(img6, template, imgShow);
 
 %% Part 2 (Computational comparisons)
 
-%Start a timer
-t = cputime;
-corrDetection(img1, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 89x78 is %d', e);
-disp(X);
+%First template evaluation
+%Top-left vertex
+xvl = 685; 
+yvl = 352;
+%Bottom-right vertex
+xvu = 774;
+yvu = 430;
 
-%Second template selection
-%Upper-left vertex
+compTime(yvl, yvu, xvl, xvu, img1 );
+
+%Second template evaluation
+%Top-left vertex
 xvl = 675; 
 yvl = 342;
 %Bottom-right vertex
 xvu = 784;
 yvu = 440;
-template = img1gray(yvl:yvu, xvl:xvu, :);
-figure, imagesc(template), title('Template [109x98]'), colormap gray
 
-%Start a timer
-t = cputime;
-corrDetection(img1, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 109x98 is %d', e);
-disp(X);
+compTime(yvl, yvu, xvl, xvu, img1 );
 
-% corrDetection(img2, template, imgShow);
-% corrDetection(img3, template, imgShow);
-% corrDetection(img4, template, imgShow);
-% corrDetection(img5, template, imgShow);
-% corrDetection(img6, template, imgShow);
-
-%Third template selection
-%Upper-left vertex
+%Third template evaluation
+%Top-left vertex
 xvl = 665; 
 yvl = 332;
 %Bottom-right vertex
 xvu = 794;
 yvu = 450;
-template = img1gray(yvl:yvu, xvl:xvu, :);
-figure, imagesc(template), title('Template [129x118]'), colormap gray
 
-%Start a timer
-t = cputime;
-corrDetection(img1, template, imgShow);
-%Time elapsed
-e = cputime - t;
-X = sprintf('The computational time for a template 129x118 is %d', e);
-disp(X);
+compTime(yvl, yvu, xvl, xvu, img1 );
 
-% corrDetection(img2, template, imgShow);
-% corrDetection(img3, template, imgShow);
-% corrDetection(img4, template, imgShow);
-% corrDetection(img5, template, imgShow);
-% corrDetection(img6, template, imgShow);
